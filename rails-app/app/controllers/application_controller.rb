d5ff0580c1466f297d1a1f9c6269ec65fa2e9a37
Karl Johansson <karl.johansson@hemnet.se>
2025-05-20 09:16:20 +0200

    Hook in rust-view into rails

--- rails-app/app/controllers/application_controller.rb
+++ rails-app/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+require_relative "../../rust-view/lib/rust_view/rust_view"
+
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def root
+    render plain: RustView.new.to_s
+  end
 end
